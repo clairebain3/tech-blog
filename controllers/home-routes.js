70bdf1570f7746e9d46187ef688eef5d3d2fb5ee
Claire Bain <claire.bain3@gmail.com>
2023-01-16 19:46:08 -0800

    working on routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,9 +5,9 @@ const { Post, Comment } = require('../models');
 router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
-        attributes: ['id'],
+        // attributes: ['id'],
         
-        //   attributes: ['title', 'content', 'created_date', 'created_by'],
+          attributes: ['title', 'content', 'created_date', 'created_by'],
         },
       
     );
@@ -26,46 +26,34 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/', async (req, res) => {
-//   // Add a comment describing the purpose of the render method
-//   // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-//   res.render('homepage');
-// });
 
 
 
-// router.get('/', function (req, res) {
-//     res.render('homepage', {});
-//   });
+// GET one post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Post,
+          attributes: [
+            'id',
+            'title',
+            'content',
+            'created_date',
+            'created_by',
+          ],
+        },
+      ],
+    });
 
-
-
-// // GET one gallery
-// router.get('/gallery/:id', async (req, res) => {
-//   try {
-//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: [
-//             'id',
-//             'title',
-//             'artist',
-//             'exhibition_date',
-//             'filename',
-//             'description',
-//           ],
-//         },
-//       ],
-//     });
-
-//     const gallery = dbGalleryData.get({ plain: true });
-//     res.render('gallery', { gallery });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    const post = dbPostData.get({ plain: true });
+    res.render('post', { post });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // // GET one painting
 // router.get('/painting/:id', async (req, res) => {
