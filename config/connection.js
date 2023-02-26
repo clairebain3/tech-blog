384214f8547eb696bdee639fafd5bbf2eb7ce526
Claire Bain <claire.bain3@gmail.com>
2023-02-26 14:33:49 -0800

    updating connection.js file for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(
+    process.env.JAWSDB_URL,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
